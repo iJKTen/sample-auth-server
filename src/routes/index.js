6e856f11092c99eb147957a10bc3b30d14653cc8
JK <49369341+iJKTen@users.noreply.github.com>
2021-02-02 21:52:45 -0500

    removed twitter passport

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,8 @@
 'use strict';
 
 const router = require('express').Router();
-const twitterRoutes = require('../api/passport/twitter');
 const googleRoutes = require('../api/passport/google');
 
-router.use('/auth/twitter', twitterRoutes);
 router.use('/auth/google', googleRoutes);
 
 module.exports = router;
