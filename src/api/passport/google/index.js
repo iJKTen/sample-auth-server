d2afdedc9ca7be1a6c145931f9df6d340cb04686
JK <49369341+iJKTen@users.noreply.github.com>
2021-02-07 16:23:16 -0500

    type in the correct url and it will work

--- src/api/passport/google/index.js
+++ src/api/passport/google/index.js
@@ -4,9 +4,10 @@
 const router = require('express').Router();
 const passport = require('./google');
 
-router.get('/', passport.authenticate('google', {
-    scope: ['https://www.googleapis.com/auth/plus.login']
-}))
+router
+    .get('/', passport.authenticate('google',
+        { scope: ['https://www.googleapis.com/auth/plus.login'] }
+    ))
     .get('/callback', passport.authenticate('google'));
 
 module.exports = router;
